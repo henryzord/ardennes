2bd6fa749c73476caf189d86c66308f089bf4f82
henryzord <henry.cagnini@gmail.com>
2017-05-16 13:29:18 -0300

    Removed a test file

--- __test__.py
+++
@@ -1,7 +0,0 @@
-from treelib.utils import DatabaseHandler
-
-dball = DatabaseHandler(
-        path='/home/henry/Projects/ardennes/metadata/iris cross-validation 2017-05-16 09:32:32.238348.db'
-)
-
-dball.plot_population()
